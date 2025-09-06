b0a26573439ef6853cbf20bee2846a1ceae0ea84
mariaeduardafernanandessoares-ui <mariaeduardafernanandessoares@gmail.com>
2025-09-06 18:17:01 -0300

    tentativa 3

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,24 +1,27 @@
-# 3 entradas  e 1 saida (tipo de triangulo).
-
+#input de dados na mesma linha
 lados = input("digite os lados do triangulos: ")
-
+#pegando as strings de entrada e convertendo para string separada
 lados_em_texto = lados.split()
 
-lados_do_triangulo = []
+lados_do_triangulo = [] #lista pra armaxzenar os valores entrados
 
-for i in lados_em_texto:
-    lado_convertido = float(i)
-    lados_do_triangulo.append(lado_convertido)
+for i in lados_em_texto: # for para coneverter cada um dos valores
+    lado_convertido = float(i)# conversao para float
+    lados_do_triangulo.append(lado_convertido)#adicionando valor convertido a lista
 
+a = lados_do_triangulo[0] #atribuçao de variavel a
+b = lados_do_triangulo[1] #atribuçao de variavel b
+c = lados_do_triangulo[2] #atribuçao de variavel c
 
-if lados_do_triangulo[0] == lados_do_triangulo[1] == lados_do_triangulo[2]:
-    print("equilátero")
-    
-elif lados_do_triangulo[0] == lados_do_triangulo[1] or lados_do_triangulo[2] == lados_do_triangulo[1] or lados_do_triangulo[2] == lados_do_triangulo[0]:
-    print("isósceles")
-
-elif lados_do_triangulo[0] != lados_do_triangulo[1] and lados_do_triangulo[0] != lados_do_triangulo[2] and lados_do_triangulo[1] != lados_do_triangulo[2]:
-    print("escaleno")
-
+#verifica se as somas dos lados são maiores que o terceiro lado, se é mesmo um triangulo.
+if (a + b > c) and (a + c > b) and (b + c > a):
+    #verificar qual o tipo de triângulo
+    if a == b == c:
+        print("equilátero")
+    elif a != b and a != c and b != c:
+        print("escaleno")
+    else: # Se não for equilátero nem escaleno, só pode ser isósceles
+        print("isósceles")
 else:
+    # Se a primeira condição for falsa, não é um triângulo
     print("Não forma triângulo")
